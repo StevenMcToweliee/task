10f203e346aad606e10772ceaf9eda0038261212
Ilgiz <mavilgiz2@yandex.ru>
2017-08-04 00:26:19 +0400

    firstLevel

--- app/js/common.js
+++ app/js/common.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
 
+  //autoheight
+  $(".sections-wrapper, .section").css("height", $(window).height());
 
-	$(".sections-wrapper, .section").css("height", $(window).height());
 
-
-          //scroll to element
-    $('.go_to').click( function(){ // ловим клик по ссылке с классом go_to
-  var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
-        if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-      $('html, body').animate({ scrollTop: $(scroll_el).offset().top}, 500); // анимируем скроолинг к элементу scroll_el
-        }
-      return false; // выключаем стандартное действие
+  //menu
+  $(".toggle-menu").click(function() {
+        $(".sw").toggleClass("active");
     });
 
 
-  var top_show = 200;
- $(window).scroll(function () { // При прокрутке попадаем в эту функцию
-     if ($(this).scrollTop() > top_show) $('.header-wrapper').addClass("header-fixed-bg");
-     else $('.header-wrapper').removeClass("header-fixed-bg");
-   });
+
+
+  //scroll to element
+  $('.go_to').click( function(){ 
+      var scroll_el = $(this).attr('href'); 
+      if ($(scroll_el).length != 0) { 
+        $('html, body').animate({ scrollTop: $(scroll_el).offset().top}, 500); 
+         }
+      return false;
+  });
+
+  //fixed menu
+ $(window).scroll(function () {
+    var top_show = 200;
+     if ($(this).scrollTop() > top_show) $(".header-wrapper").addClass("header-fixed-bg");
+     else $(".header-wrapper").removeClass("header-fixed-bg");
+});
+
+ //animate
+$(".sw").animated("fadeInDown", "fadeInDown");
+$(".top-nav-list").animated("fadeInDown", "fadeInDown");
+$(".text-title").animated("fadeInRight", "fadeInRight");
+$(".slider-title h2").animated("fadeInDown", "fadeInDown");
+$(".slider-main").animated("fadeInUp", "fadeInUp");
+
 });
